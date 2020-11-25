bf5c6e2d30951a28c22c33d0ed460823aabd6c70
xuezonggui <1301834367@qq.com>
2020-11-25 20:36:46 +0800

    add 1370(2)

--- src/sort/1370. Increasing Decreasing String/test.go
+++ src/sort/1370. Increasing Decreasing String/test.go
@@ -36,3 +36,27 @@ func sortString(s string) string {
 	}
 	return string(ns)
 }
+
+//with map
+func sortString2(s string) string {
+	m := make([]int, 'z'+1)
+	for _, v := range s {
+		m[v]++
+	}
+	ns := make([]byte, 0)
+	for len(ns) < len(s) {
+		for i := 'a'; i <= 'z'; i++ {
+			if m[i] > 0 {
+				ns = append(ns, byte(i))
+				m[i]--
+			}
+		}
+		for j := 'z'; j >= 'a'; j-- {
+			if m[j] > 0 {
+				ns = append(ns, byte(j))
+				m[j]--
+			}
+		}
+	}
+	return string(ns)
+}
