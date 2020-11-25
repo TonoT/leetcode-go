25937e6a6f50b7d06c6eff06a698a2891d0585d5
xuezonggui <1301834367@qq.com>
2020-11-25 15:28:48 +0800

    add 222（2）

--- src/tree/222. Count Complete Tree Nodes/test.go
+++ src/tree/222. Count Complete Tree Nodes/test.go
@@ -21,3 +21,58 @@ func countNodes(root *TreeNode) int {
 	return left + right + 1
 
 }
+
+//bfs
+func countNodes2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	s := Constructor()
+	sum := 0
+	s.Push(root)
+	for !s.Empty() {
+		for i := 0; i < len(s.stack); i++ {
+			pop := s.Pop()
+			sum++
+			if pop.Left != nil {
+				s.Push(pop.Left)
+			}
+			if pop.Right != nil {
+				s.Push(pop.Right)
+			}
+		}
+
+	}
+	return sum
+}
+
+type MyStack struct {
+	stack []*TreeNode
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyStack {
+	return MyStack{stack: make([]*TreeNode, 0)}
+}
+
+/** Push element x onto stack. */
+func (this *MyStack) Push(x *TreeNode) {
+	this.stack = append(this.stack, x)
+}
+
+/** Removes the element on top of the stack and returns that element. */
+func (this *MyStack) Pop() *TreeNode {
+	ele := this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	return ele
+}
+
+/** Get the top element. */
+func (this *MyStack) Top() *TreeNode {
+	return this.stack[len(this.stack)-1]
+}
+
+/** Returns whether the stack is empty. */
+func (this *MyStack) Empty() bool {
+	return len(this.stack) == 0
+}
