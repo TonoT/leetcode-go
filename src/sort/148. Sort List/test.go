545b42aecdcfb2781256f1b5d7a5abfb60935566
xuezonggui <1301834367@qq.com>
2020-11-29 18:13:55 +0800

    add 148

--- src/sort/148. Sort List/test.go
+++ src/sort/148. Sort List/test.go
@@ -43,3 +43,45 @@ func sortList(head *ListNode) *ListNode {
 	}
 	return v.Next
 }
+
+func sortList1(head *ListNode) *ListNode {
+
+	return sort1(head, nil)
+}
+
+func sort1(first, second *ListNode) *ListNode {
+	if first == nil {
+		return first
+	}
+
+	if first.Next == second {
+		first.Next = nil
+		return first
+	}
+	slow, fast := first, first
+	for fast != second {
+		slow = slow.Next
+		fast = fast.Next
+		if fast != second {
+			fast = fast.Next
+		}
+	}
+	mid := slow
+	return mergeTwoLists(sort1(first, mid), sort1(mid, second))
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	s1 := &ListNode{}
+	node := s1
+	for l1 != nil || l2 != nil {
+		if l1 != nil && (l2 == nil || l1.Val < l2.Val) {
+			node.Next = l1
+			l1 = l1.Next
+		} else {
+			node.Next = l2
+			l2 = l2.Next
+		}
+		node = node.Next
+	}
+	return s1.Next
+}
