3afa54cb05595987869005bb21b5b6c47426604c
xuezonggui <1301834367@qq.com>
2020-12-01 20:10:04 +0800

    add 34

---
+++ src/sort/34. Find First and Last Position of Element in Sorted Array/test.go
@@ -0,0 +1,15 @@
+package main
+
+import "sort"
+
+func main() {
+
+}
+func searchRange(nums []int, target int) []int {
+	l := sort.SearchInts(nums, target)
+	if l == len(nums) || nums[l] != target {
+		return []int{-1, -1}
+	}
+	r := sort.SearchInts(nums, target+1) - 1
+	return []int{l, r}
+}
