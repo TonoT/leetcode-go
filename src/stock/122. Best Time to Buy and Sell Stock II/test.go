12abb5a6ae251bc6218561ac4c864f29deeaab3c
xuezonggui <1301834367@qq.com>
2020-12-28 15:19:15 +0800

    add 122 dynamic

--- src/stock/122. Best Time to Buy and Sell Stock II/test.go
+++ src/stock/122. Best Time to Buy and Sell Stock II/test.go
@@ -15,3 +15,20 @@ func maxProfit(prices []int) int {
 	}
 	return sum
 }
+func maxProfit2(prices []int) int {
+	n := len(prices)
+	buy := make([]int, n)
+	sell := make([]int, n)
+	buy[0] = -prices[0]
+	for i := 1; i < n; i++ {
+		buy[i] = max(buy[i-1], sell[i-1]-prices[i])
+		sell[i] = max(sell[i-1], buy[i-1]+prices[i])
+	}
+	return sell[n-1]
+}
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
