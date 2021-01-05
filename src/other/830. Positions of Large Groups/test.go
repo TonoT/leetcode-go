3650a29850f8f51dfb8a618a8b17d8efac7ab34c
xuezonggui <1301834367@qq.com>
2021-01-05 19:35:48 +0800

    add 830

--- src/other/830. Positions of Large Groups/test.go
+++ src/other/830. Positions of Large Groups/test.go
@@ -31,3 +31,17 @@ func largeGroupPositions(s string) [][]int {
 	}
 	return m
 }
+
+func largeGroupPositions2(s string) [][]int {
+	start := 0
+	var r [][]int
+	for i := 0; i <= len(s); i++ {
+		if i == len(s) || s[start] != s[i] {
+			if i-start >= 3 {
+				r = append(r, []int{start, i - 1})
+			}
+			start = i
+		}
+	}
+	return r
+}
