87f2956873f5d45dd3e4b5b44bea14959753fddf
xuezonggui <1301834367@qq.com>
2021-01-21 14:11:29 +0800

    add 1584 prim

--- src/union find/1584. Min Cost to Connect All Points/test.go
+++ src/union find/1584. Min Cost to Connect All Points/test.go
@@ -3,7 +3,13 @@ package main
 import "sort"
 
 func main() {
-
+	minCostConnectPoints2([][]int{
+		{0, 0},
+		{2, 2},
+		{3, 10},
+		{5, 2},
+		{7, 0},
+	})
 }
 func minCostConnectPoints(points [][]int) int {
 	parent := make([]int, len(points))
@@ -52,4 +58,37 @@ func abs(i int) int {
 		return -i
 	}
 	return i
+}
+
+//prim
+func minCostConnectPoints2(points [][]int) int {
+	type point struct {
+		pot int
+		dis int
+	}
+	distance := make([]point, 0)
+	//记录总长
+	all := 0
+	for i := 1; i < len(points); i++ {
+		distance = append(distance, point{
+			pot: i,
+			dis: abs(points[i][0]-points[0][0]) + abs(points[i][1]-points[0][1]),
+		})
+	}
+
+	for len(distance) > 0 {
+		sort.Slice(distance, func(i, j int) bool {
+			return distance[i].dis < distance[j].dis
+		})
+		all += distance[0].dis
+		next := distance[0].pot
+		for i := 1; i < len(distance); i++ {
+			dis := abs(points[distance[i].pot][0]-points[next][0]) + abs(points[distance[i].pot][1]-points[next][1])
+			if dis < distance[i].dis {
+				distance[i].dis = dis
+			}
+		}
+		distance = distance[1:]
+	}
+	return all
 }
